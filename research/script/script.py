a04721efd174dada48ad468b45443a45d3e495cc
Federico Mon <gnufede@gmail.com>
2022-09-26 14:59:47 +0200

    wap

--- research/script/script.py
+++ research/script/script.py
@@ -1,20 +1,5 @@
 from sample_headers import HEADERS
-
-valid_user_agent_header_formats = (
-    "useragent",
-    "user-agent",
-    "http-user-agent",
-    "request-user-agent",
-    "http-request-user-agent",
-)
-
-
-def find_user_agent_header(headers):
-    for k, v in headers.items():
-        for user_agent_format in valid_user_agent_header_formats:
-            if k.lower().replace("_", "-") == user_agent_format:
-                return v
-    return None
+from find_headers import find_user_agent_header
 
 
 if __name__ == "__main__":
--- research/script/setup.py
+++ research/script/setup.py
@@ -3,7 +3,6 @@ from Cython.Build import cythonize
 
 setup(
     name="Hello world app",
-    # ext_modules=cythonize("cyscript.pyx"),
     ext_modules=cythonize("cyscript2.pyx", "_cystring.pxd"),
     language="C++",
     zip_safe=False,
